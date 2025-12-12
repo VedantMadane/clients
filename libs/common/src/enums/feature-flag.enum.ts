c4c019b62b8efc1100c9fb901e1223d53910df2d
rr-bw <102181210+rr-bw@users.noreply.github.com>
2025-12-12 14:22:42 -0800

    [PM-27086] add flagged logic to InputPasswordComponent.submit()

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -131,6 +131,7 @@ export const DefaultFeatureFlagValue = {
 
   /* Auth */
   [FeatureFlag.PM23801_PrefetchPasswordPrelogin]: FALSE,
+  [FeatureFlag.PM27086_UpdateAuthenticationApisForInputPassword]: FALSE,
 
   /* Billing */
   [FeatureFlag.TrialPaymentOptional]: FALSE,
