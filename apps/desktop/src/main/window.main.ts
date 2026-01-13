7b11ee03520e5d2c6a8cd05963d15212384d3555
Isaiah Inuwa <iinuwa@bitwarden.com>
2026-01-13 12:52:30 -0600

    Apply lints

--- apps/desktop/src/main/window.main.ts
+++ apps/desktop/src/main/window.main.ts
@@ -557,8 +557,8 @@ export class WindowMain {
 }
 
 export type WindowDetails = {
-  isVisible: boolean,
-  isFocused: boolean,
+  isVisible: boolean;
+  isFocused: boolean;
   // Base64-encoded native handle
-  handle: Buffer,
-}
+  handle: Buffer;
+};
